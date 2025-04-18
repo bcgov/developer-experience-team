bfac9acfb2f4ab4b380024a8211877f716879e0c
Shea Phillips <shea.phillips@gmail.com>
2025-04-17 17:17:43 -0700

    Various updates - update README - add functionality to remove users from an org when there is no matching Entra ID identity - add parameters for org and controlling whether users are removed from an org automatically

--- utils/idir/check_idir_status.py
+++ utils/idir/check_idir_status.py
@@ -3,50 +3,88 @@ import asyncio
 from azure.identity import InteractiveBrowserCredential
 from msgraph import GraphServiceClient
 from msgraph.generated.models.reference_create import ReferenceCreate
-import sys
+from github import Github, Auth
+
+import sys, os, re, datetime, argparse
+
 
 from msgraph.generated.users.users_request_builder import UsersRequestBuilder
 
+def remove_github_user_from_org(github_client, github_id, org_name):
+    org = github_client.get_organization(org_name)
+    try:
+        user = github_client.get_user(github_id)
+        if user:
+            org.remove_from_members(user)
+            return True
+        else:
+            print(f"User {github_id} not found in  {org_name}.")
+            return False
+    except Exception as e:
+        print(f"Error removing {github_id} from {org_name}: {e}")
+        return False
 
-async def find_user_by_email(client, email):
-	query_params = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
-		filter=f"mail eq '{email}'",
-	)
+async def search(client, email):
+    query_params = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(search=[f'("mail:{email}" OR "userPrincipalName:{email}" OR "otherMails:{email}")'], select=["id", "displayName", "userPrincipalName", "mail","otherMails"],
+    )
 
-	request_configuration = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
-		query_parameters=query_params,
-	)
+    request_configuration = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
+        query_parameters=query_params,
+    )
 
-	users = await client.users.get(request_configuration=request_configuration)
-	if users:
-		return users.value
+    request_configuration.headers.add("ConsistencyLevel", "eventual")
 
-
-async def find_user_by_user_principal(client, email):
-	return await client.users.by_user_id(email).get()
-
+    users = await client.users.get(request_configuration=request_configuration)
+    if users:
+        return users.value
 
 async def main():
-	credential = InteractiveBrowserCredential()
-	client = GraphServiceClient(credentials=credential)
+	# Parse command line arguments
+    parser = argparse.ArgumentParser(description="Correlate GitHub SSO identity in a GitHub organization with Entra ID accounts and optionally remove users from the organization.")
+    parser.add_argument("org", help="GitHub organization name")
+    parser.add_argument("-r","--remove", help="Whether to remove users from a GitHub organization when there is no Entra ID account matching their GitHub SSO identity.", action="store_true")
+    args = parser.parse_args()
 
-	for line in sys.stdin:
-		user_email = line.strip()
-		user_found = False
-		try:
-			user = await find_user_by_user_principal(client, user_email)
+	# Map command line arguments to local variables
+    remove_unmatched_users = args.remove
+    github_org = args.org
 
-			if user:
-				user_found = True
+    # authenticate with Graph API in order to search for users in Entra ID
+    credential = InteractiveBrowserCredential()
+    client = GraphServiceClient(credentials=credential)
 
-		except Exception as e:
-			users = await find_user_by_email(client, user_email)
-			if users:
-				for user in users:
-					if user.mail == user_email:
-						user_found = True
+    # authenticate with GitHub API
+    auth = Auth.Token(os.environ['GITHUB_TOKEN'])
+    github_client = Github(auth=auth)
 
-		print(f"{user_email}, {user_found}")
+    # read records from stdin, where each represent a GitHub user in the the organization
+    for line in sys.stdin:
+        line_parts = line.strip().split(",")
+        user_email = line_parts[0].strip('\"')
+        github_id = line_parts[1].strip('\"')
 
+        removed_from_org = False
+        message = ""
+        has_idir = False
+
+        if user_email:
+            # check if user_email is an email address using regex
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", user_email):
+                message = "Unexpected NameID format."
+            else:
+                users = await search(client, user_email)
+
+                if not users:
+                    has_idir = False
+                    if remove_unmatched_users:
+                        removed_from_org = remove_github_user_from_org(github_client, github_id, github_org)
+                else:
+                    has_idir = True
+        else:
+            user_email = "NO EMAIL??!!"
+            message = "NameID is empty."
+
+        if not has_idir:
+            print(f"{datetime.datetime.today().strftime('%d-%m-%Y')}, {github_org}, {user_email}, {github_id}, {has_idir}, {removed_from_org},,{message}")
 
 asyncio.run(main())
--- utils/idir/check_users.sh
+++ utils/idir/check_users.sh
@@ -1,6 +1,19 @@
 #!/bin/zsh
+ ORG=$1
+ REMOVE=$2
 
- gh api graphql --paginate -F org=bcgov -f query='query($org: String!, $endCursor:String) {
+# Check if the org name is provided
+if [ -z "$ORG" ]; then
+  echo "Usage: $0 <org_name> [remove]"
+  exit 1
+fi
+
+# Check if remove flag is provided. If it is, set REMOVE_FLAG to "-r" otherwise set it to an empty string
+if [ "$REMOVE"=="remove" ]; then
+  REMOVE_FLAG="-r"
+fi
+
+ gh api graphql --paginate -F org=$ORG -f query='query($org: String!, $endCursor:String) {
   organization(login: $org) {
     samlIdentityProvider {
       externalIdentities(first: 100, after: $endCursor) {
@@ -25,4 +38,4 @@
       }
     }
   }
-}' | jq -r '.data.organization.samlIdentityProvider.externalIdentities.edges[] | .node.samlIdentity.nameId' | python check_idir_status.py | grep False
+}' | jq  -r  '.data.organization.samlIdentityProvider.externalIdentities.edges[] | [.node.samlIdentity.nameId, .node.user.login] | @csv' | python check_idir_status.py $ORG $REMOVE_FLAG
