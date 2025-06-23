69f77450c6c55217f34274149a8269e87dfc4770
Monica Granbois <MonicaG@users.noreply.github.com>
2025-06-23 14:22:45 -0700

    initial upgrade updates for test mobile app

--- DevExMobileTestApp/App.tsx
+++ DevExMobileTestApp/App.tsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 
 class App extends Component {
   state = {
