f31f692a8a9db0dcfcd4922a5a5726e036d0478d
Monica Granbois <MonicaG@users.noreply.github.com>
2025-11-10 14:17:26 -0800

    Ignore questions with ignored tags for share links query

--- utils/stackoverflow/so_explore.py
+++ utils/stackoverflow/so_explore.py
@@ -17,23 +17,35 @@ import duckdb as dd
 # dd.sql("select answer_id from (select answers from './questions_answers_comments.json')").show()
 # dd.sql("select users.email, count(*) as answer_count from (select unnest(questions.answers, recursive := true) from './questions_answers_comments.json' as questions) as answers, './users.json' as users where answers.user_id = users.user_id group by users.email order by answer_count desc").show()
 # dd.sql("select tags.name, tags.count, age(to_timestamp(tags.last_activity_date)) from './tags.json' as tags order by tags.last_activity_date desc, tags.count desc").show()
-# dd.sql("select questions.question_id, questions.title, from './discussions_to_add.json' as questions where list_contains(questions.tags, 'openshift')").show()
+#  dd.sql("select questions.question_id, questions.title, from './questions_answers_comments.json' as questions where not list_contains(questions.tags, 'importer-ignore') and not list_contains(questions.tags, 'kibana')").show()
 # dd.sql("select questions.question_id, questions.title, questions.tags from './discussions_to_add.json' as questions order by questions.question_id").show()
 # dd.sql("select tags.name, tags.count, age(to_timestamp(tags.last_activity_date)) from './tags.json' as tags where tags.count < 2 order by tags.last_activity_date desc, tags.count desc").show()
 # dd.sql("select question_id, view_count from './questions_answers_comments.json' as questions where view_count >= 100 order by view_count desc").show()
 # dd.sql("select questions.answers.comments.link, questions.answers.link, questions.answers.share_link, questions.link, questions.share_link from './questions_answers_comments.json' as questions").show()
 # dd.sql("select unnest(questions.comments).link, unnest(questions.answers).share_link, unnest(questions.answers).link, unnest(questions.answers.comments).link, questions.link, questions.share_link from './questions_answers_comments.json' as questions").show()
-dd.sql("select tags.name, tags.count from './tags.json' as tags where tags.count < 5").show()
+# dd.sql("select tags.name, tags.count from './tags.json' as tags where tags.count < 5").show()
 
+# dd.sql("select question_id, title, tags from  './questions_answers_comments.json' as questions where questions.question_id in (select question_id from (select unnest(questions.answers, recursive := true) from './questions_answers_comments.json' as questions) as answers where answers.body like '%<img%' UNION " \
+       # "select question_id from './questions_answers_comments.json' as questions where questions.body like '%<img%')").show(max_rows=500)
+
+# dd.sql("select question_id from (select unnest (answers.comments, recursive := true) from (select unnest(questions.answers, recursive := true) from './questions_answers_comments.json' as questions) as answers ) as comments where comments.body like '%<img%'").show()
+
+# dd.sql("select * from (select answers.comments from (select unnest(questions.answers, recursive := true) from './questions_answers_comments.json' as questions) as answers) as comments where comments.edited = 'false'").show()
+
+# dd.sql("select question_id, title from  './questions_answers_comments.json' as questions where questions.question_id in (select question_id from (select unnest(questions.answers, recursive := true) from './questions_answers_comments.json' as questions) as answers where answers.body like '%<img%')").show(max_rows=500)
+# dd.sql("select question_id from './questions_answers_comments.json' as questions where questions.body like '%<img%'").show(max_rows=500)
 # This gets all links and share_links from answers, questions and comments and outputs them to a file. User links are ignored
-#dd.sql("Copy (" \
-#"select unnest(answers.comments).link from (select unnest(questions.answers, recursive:=true) from './questions_answers_comments.json' as questions) as answers where answers.question_id " \
-#"UNION select unnest(questions.answers).link from './questions_answers_comments.json' as questions where questions.question_id " \
-#"UNION select unnest(questions.answers).share_link from './questions_answers_comments.json' as questions where questions.question_id " \
-#"UNION select questions.link from './questions_answers_comments.json' as questions where questions.question_id " \
-#"UNION select questions.share_link from './questions_answers_comments.json' as questions where questions.question_id " \
-#"UNION select unnest(questions.comments).link from './questions_answers_comments.json' as questions where questions.question_id " \
-#  ")to 'link_query_output.csv' (HEADER false )")
+dd.sql("Copy (" \
+"select unnest(answers.comments).link from (select unnest(questions.answers, recursive:=true) from './questions_answers_comments.json' as questions where not list_contains(questions.tags, 'importer-ignore') and not list_contains(questions.tags, 'kibana')) as answers where answers.question_id " \
+"UNION select unnest(questions.answers).link from './questions_answers_comments.json' as questions where questions.question_id and not list_contains(questions.tags, 'importer-ignore') and not list_contains(questions.tags, 'kibana') " \
+"UNION select unnest(questions.answers).share_link from './questions_answers_comments.json' as questions where questions.question_id and not list_contains(questions.tags, 'importer-ignore') and not list_contains(questions.tags, 'kibana') " \
+"UNION select questions.link from './questions_answers_comments.json' as questions where questions.question_id and not list_contains(questions.tags, 'importer-ignore') and not list_contains(questions.tags, 'kibana') " \
+"UNION select questions.share_link from './questions_answers_comments.json' as questions where questions.question_id and not list_contains(questions.tags, 'importer-ignore') and not list_contains(questions.tags, 'kibana') " \
+"UNION select unnest(questions.comments).link from './questions_answers_comments.json' as questions where questions.question_id and not list_contains(questions.tags, 'importer-ignore') and not list_contains(questions.tags, 'kibana') " \
+ ")to 'link_query_output.csv' (HEADER false )")
 
 
 #  ggrep -Pio '"(link|share_link)":\s*"https://stackoverflow.developer.gov.bc.ca/((?!users).*)"'  questions_answers_comments.json | ggrep -Pio 'http.*\d' > links_from_json_file.txt
+
+
+# dd.sql("select unnest(answers.comments).link from (select unnest(questions.answers, recursive:=true) from './questions_answers_comments.json' as questions where ) as answers where answers.question_id").show()
